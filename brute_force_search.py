26c02de3aa0a0f3cc8c79de56014bac385c00d32
Fernanda Borges da Silva <ferfacee@gmail.com>
2019-07-20 16:46:59 -0300

    Incluindo voto majoritario na funcao fit e armazenamento em csv de todas as iteracoes da busca

--- brute_force_search.py
+++ brute_force_search.py
@@ -54,35 +54,55 @@ class BruteForceEnsembleClassifier:
                     params = dict(zip(keys, v))
                     yield (estimator, params)
                     
+    def fit_ensemble(self, X, y, ensemble, best_fitness_classifiers):
+        classifiers_fitness_it = 0
+        for estimator, params in ensemble:
+            estimator.set_params(**params)
+            self.ensemble.append(Estimator(classifier=estimator, random_state=self.random_state, fitness=best_fitness_classifiers[classifiers_fitness_it]))
+            classifiers_fitness_it = classifiers_fitness_it + 1
+        for classifier in self.ensemble:
+            classifier.fit(X, y)
+        
     def fit(self, X, y):
+        result_dict = dict()
         random.seed(self.random_state)
         kf = KFold(n_splits=5, random_state=self.random_state)
-        start_time = int(round(time.time() * 1000))
-        best_fitness = np.zeros([self.n_estimators])
+        best_ensemble_fitness = np.zeros([len(y)])
+        best_fitness_classifiers = np.zeros([self.n_estimators])
         for i, classifiers in enumerate(combinations(self.estimators_pool(self.algorithms),self.n_estimators)):
-            predictions = np.zeros([self.n_estimators])
+            # a matrix with all observations vs the prediction of each classifier
+            classifiers_predictions = np.zeros([len(y),self.n_estimators])
+            # sum the number of right predictions for each classifier
+            classifiers_right_predictions = np.zeros([self.n_estimators])
+            ensemble_fitness = np.zeros([len(y)])
             classifier_id = 0
             if i >= self.stop_time:
                 break
             for classifier, params in classifiers:
                 classifier.set_params(**params)
                 count_right_answers = 0
+                #k-fold cross-validation
                 for train, val in kf.split(X):
                     classifier.fit(X[train], y[train])
-                    count_right_answers = count_right_answers + np.equal(classifier.predict(X[val]), y[val]).sum()
-                predictions[classifier_id] = count_right_answers
+                    y_pred = classifier.predict(X[val])
+                    count_right_answers = count_right_answers + np.equal(y_pred, y[val]).sum()
+                    for observation, result in enumerate(y_pred):
+                        classifiers_predictions[observation][classifier_id] = result
+                classifiers_right_predictions[classifier_id] = count_right_answers
                 classifier_id = classifier_id + 1
-            if(predictions.sum() > best_fitness.sum()):
-                best_fitness = predictions
+            #the ensemble make the final prediction by majority vote for accuracy
+            majority_voting = stats.mode(classifiers_predictions, axis=1)[0]
+            majority_voting = [int(j[0]) for j in majority_voting]
+            for iterator in range(0, len(y)):
+                ensemble_fitness[iterator] = np.equal(majority_voting[iterator],y[iterator])
+            #select the most accurate ensemble
+            if(ensemble_fitness.sum() > best_ensemble_fitness.sum()):
+                best_ensemble_fitness = ensemble_fitness
+                best_fitness_classifiers = classifiers_right_predictions
                 ensemble = classifiers
-        best_fitness_it = 0
-        for estimator, params in ensemble:
-            estimator.set_params(**params)
-            self.ensemble.append(Estimator(classifier=estimator, random_state=self.random_state, fitness=best_fitness[best_fitness_it]))
-            best_fitness_it = best_fitness_it + 1
-        for classifier in self.ensemble:
-            classifier.fit(X, y)
-        return ensemble
+            end_time = time.strftime("%H:%M:%S", time.localtime(time.time()))
+            result_dict.update({i: {"end_time":end_time,"best_fitness_ensemble":best_ensemble_fitness.sum(), "ensemble":ensemble, "best_fitness_classifiers":best_fitness_classifiers}})
+        return result_dict
     
     def predict(self, X):
         predictions = np.zeros((self.n_estimators, len(X)))
