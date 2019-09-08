68838733cad5ce37745fb67dbfd1cfc159c8c2ac
Fernanda Borges da Silva <ferfacee@gmail.com>
2019-09-08 19:11:23 -0300

    corrigindo parallel_diversity_ensemble.py

--- exec/parallel_diversity_ensemble.py
+++ exec/parallel_diversity_ensemble.py
@@ -112,12 +112,23 @@ class DiversityEnsembleClassifier:
         for i in range(0, population_size):
             self.population.append(Chromossome(genotypes_pool=algorithms, random_state=random_state))
 
-    def generate_offspring(self, parents, children):
+    def generate_offspring(self, parents, children, pop_fitness):
+        children_aux = children
         if not parents:
             parents = [x for x in range(0, self.population_size)]
             children = [x for x in range(self.population_size, 2*self.population_size)]
-        for i in range(0, len(children)):
-            new_chromossome = copy.deepcopy(self.population[parents[i%len(parents)]])
+        
+        if (len(parents) < self.population_size):
+            diff = self.population_size - len(parents)
+            for i in range(0, diff):
+                max_fit_index_aux = np.argmax(pop_fitness)
+                self.population[children_aux[i]] = copy.deepcopy(self.population[max_fit_index_aux])
+                parents.append(children_aux[i])
+                pop_fitness = np.delete(pop_fitness, max_fit_index_aux)
+                children = np.delete(children, i)
+                
+        for i in range(0, self.population_size):
+            new_chromossome = copy.deepcopy(self.population[parents[i]])
             new_chromossome.mutate()
             try:
                 self.population[children[i]] = new_chromossome
@@ -154,9 +165,9 @@ class DiversityEnsembleClassifier:
             selected.append(target_chromossome)
             self.population[target_chromossome].fitness = pop_fitness[target_chromossome]
 
-        return selected, (diversity[selected]/self.population_size).mean(), mean_fitness/(self.population_size)
+        return selected, (diversity[selected]/self.population_size).mean(), mean_fitness/(self.population_size), pop_fitness
 
-    def fit(self, X, y):
+    def fit(self, X, y, n_cores):
         diversity_values, fitness_values = [], []
         result_dict = dict()
         ##print('Starting genetic algorithm...')
@@ -165,38 +176,51 @@ class DiversityEnsembleClassifier:
         random.seed(self.random_state)
 
         selected, not_selected = [], []
-        all_predictions = np.empty([2*self.population_size, y.shape[0]])
+        selected, not_selected, pop_fitness = [], [], []
+        all_predictions = np.zeros([2*self.population_size, y.shape[0]])
 
         frequencies = np.unique(y, return_counts=True)[1]
         selection_threshold = max(frequencies)/np.sum(frequencies)
 
         for epoch in range(self.max_epochs):
+            now = time.time()
+            struct_now = time.localtime(now)
+            mlsec = repr(now).split('.')[1][:3]
+            start_time = time.strftime("%Y-%m-%d %H:%M:%S.{} %Z".format(mlsec), struct_now)
+            time_aux = int(round(now * 1000))
+            
             ensemble = []
             classifiers_fitness = []
 
             not_selected = np.setdiff1d([x for x in range(0, 2*self.population_size)], selected)
-            self.generate_offspring(selected, not_selected)
-            #predictions = self.fit_predict_population(not_selected, predictions, kf, X, y)
+            self.generate_offspring(selected, not_selected, pop_fitness)
             
-            num_cores = multiprocessing.cpu_count()
             backend = 'loky'
-            fit_predictions = Parallel(n_jobs=num_cores, backend=backend)(delayed(self.fit_predict_population)(item, kf, X, y) for item in not_selected)
+            fit_predictions = Parallel(n_jobs=n_cores, backend=backend)(delayed(self.fit_predict_population)(item, kf, X, y) for item in not_selected)
             
             for i in fit_predictions:
                 all_predictions[i[0]] = i[1]
 
             #print('Applying diversity selection...', end='')
-            selected, diversity, fitness = self.diversity_selection(all_predictions, selection_threshold)
+            selected, diversity, fitness, pop_fitness = self.diversity_selection(all_predictions, selection_threshold)
             diversity_values.append(diversity)
             fitness_values.append(fitness)
             for chromossome in self.population:
                 ensemble.append(chromossome.classifier)
                 classifiers_fitness.append(chromossome.fitness)
+                
             now = time.time()
             struct_now = time.localtime(now)
             mlsec = repr(now).split('.')[1][:3]
             end_time = time.strftime("%Y-%m-%d %H:%M:%S.{} %Z".format(mlsec), struct_now)
-            result_dict.update({epoch:{"end_time":end_time,"diversity":diversity,"fitness":fitness,"ensemble":ensemble, "classifiers_fitness":classifiers_fitness}})
+            total_time = (int(round(now * 1000)) - time_aux)
+            result_dict.update({epoch:{"start_time":start_time,
+                                       "end_time":end_time,
+                                       "total_time_ms":total_time,
+                                       "diversity":diversity,
+                                       "fitness":fitness,
+                                       "ensemble":ensemble, 
+                                       "classifiers_fitness":classifiers_fitness}})
         return result_dict
     
     def fit_ensemble(self, X, y, ensemble, classifiers_fitness):
@@ -209,8 +233,8 @@ class DiversityEnsembleClassifier:
 
     def predict(self, X):
         len_X = len(X)
-        predictions = np.empty((self.population_size, len_X))
-        y = np.empty(len_X)
+        predictions = np.zeros((self.population_size, len_X))
+        y = np.zeros(len_X)
         for chromossome in range(0, self.population_size):
             predictions[chromossome] = self.ensemble[chromossome].predict(X)
         for i in range(0, len_X):
@@ -223,7 +247,7 @@ class DiversityEnsembleClassifier:
             y[i] = max(pred.items(), key=operator.itemgetter(1))[0]
         return y
     
-def compare_results(data, target, n_estimators, outputfile, stop_time):
+def compare_results(data, target, n_estimators, outputfile, stop_time, n_cores):
     accuracy, f1, precision, recall, auc = 0, 0, 0, 0, 0
     total_accuracy, total_f1, total_precision, total_recall, total_auc = 0, 0, 0, 0, 0
     div = np.zeros(stop_time)
@@ -255,11 +279,13 @@ def compare_results(data, target, n_estimators, outputfile, stop_time):
         for i in range(0, 10):
             csv_file = 'parallel_diversity_results_iter_' + str(i) + '_' + time.strftime("%H_%M_%S", time.localtime(time.time())) + '.csv'
             X_train, X_test, y_train, y_test = train_test_split(data, target, test_size=0.2, random_state=i*10)
-            ensemble_classifier = DiversityEnsembleClassifier(algorithms=alg, population_size=n_estimators, max_epochs=stop_time, random_state=i*10)
+            ensemble_classifier = DiversityEnsembleClassifier(algorithms=alg, 
+                                                              population_size=n_estimators, 
+                                                              max_epochs=stop_time, random_state=i*10)
             print('\n\nIteration = ',i)
             text_file.write("\n\nIteration = %i" % (i))
             fit_aux = int(round(time.time() * 1000))
-            results = ensemble_classifier.fit(X_train, y_train)
+            results = ensemble_classifier.fit(X_train, y_train, n_cores)
             results_pd = pd.DataFrame.from_dict(results, orient='index')
             results_pd.to_csv (csv_file, index = None, header=True)
             ensemble = results_pd[-1:]["ensemble"].item()
@@ -317,16 +343,16 @@ def main(argv):
     stop_time = ''
     save_results = 'diversity_ensemble_results_' + time.strftime("%H_%M_%S", time.localtime(time.time())) + ".csv"
     try:
-        opts, args = getopt.getopt(argv,"h:i:o:e:s:",["ifile=","ofile=","enumber=","stoptime="])
+        opts, args = getopt.getopt(argv,"h:i:o:e:s:c:",["ifile=","ofile=","enumber=","stoptime=","cores="])
     except getopt.GetoptError:
-        print('parallel_diversity_ensemble.py -i <inputfile> -o <outputfile> -e <n_estimators> -s <stop_time>')
+        print('parallel_diversity_ensemble.py -i <inputfile> -o <outputfile> -e <n_estimators> -s <stop_time> -c <n_cores>')
         sys.exit(2)
     if opts == []:
-        print('parallel_diversity_ensemble.py -i <inputfile> -o <outputfile> -e <n_estimators> -s <stop_time>')
+        print('parallel_diversity_ensemble.py -i <inputfile> -o <outputfile> -e <n_estimators> -s <stop_time> -c <n_cores>')
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
-            print('parallel_diversity_ensemble.py -i <inputfile> -o <outputfile> -e <n_estimators> -s <stop_time>')
+            print('parallel_diversity_ensemble.py -i <inputfile> -o <outputfile> -e <n_estimators> -s <stop_time> -c <n_cores>')
             sys.exit()
         elif opt in ("-i", "--ifile"):
             inputfile = arg
@@ -336,28 +362,55 @@ def main(argv):
             n_estimators = arg
         elif opt in ("-s", "--stoptime"):
             stop_time = arg
+        elif opt in ("-c", "--cores"):
+            n_cores = arg
     print('Input file is ', inputfile)
     print('Output file is ', outputfile)
     print('The number of estimators is ', n_estimators)
     print('The number of iterations is ', stop_time)
+    print('The number of cores is ', n_cores)
     if inputfile == "iris":
         dataset = datasets.load_iris()
         print('Runing Diversity-based Ensemble Classifier (parallel version)...')
-        compare_results(data=dataset.data, target=dataset.target, n_estimators=int(n_estimators), outputfile=outputfile, stop_time=int(stop_time))
+        compare_results(data=dataset.data, 
+                        target=dataset.target, 
+                        n_estimators=int(n_estimators), 
+                        outputfile=outputfile, 
+                        stop_time=int(stop_time),
+                        n_cores=int(n_cores)
+                       )
     elif inputfile == "breast":
         dataset = datasets.load_breast_cancer()
         print('Runing Diversity-based Ensemble Classifier (parallel version)...')
-        compare_results(data=dataset.data, target=dataset.target, n_estimators=int(n_estimators), outputfile=outputfile, stop_time=int(stop_time))
+        compare_results(data=dataset.data, 
+                        target=dataset.target, 
+                        n_estimators=int(n_estimators), 
+                        outputfile=outputfile, 
+                        stop_time=int(stop_time),
+                        n_cores=int(n_cores)
+                       )
     elif  inputfile == "wine":
         dataset = datasets.load_wine()
         print('Runing Diversity-based Ensemble Classifier (parallel version)...')
-        compare_results(data=dataset.data, target=dataset.target, n_estimators=int(n_estimators), outputfile=outputfile, stop_time=int(stop_time))
+        compare_results(data=dataset.data, 
+                        target=dataset.target, 
+                        n_estimators=int(n_estimators), 
+                        outputfile=outputfile, 
+                        stop_time=int(stop_time),
+                        n_cores=int(n_cores)
+                       )
     else:
         le = LabelEncoder()
         dataset = pd.read_csv(inputfile)
         dataset.iloc[:, -1] = le.fit_transform(dataset.iloc[:, -1])
         print('Runing Diversity-based Ensemble Classifier (parallel version)...')
-        compare_results(data=dataset.iloc[:, 0:-1].values, target=dataset.iloc[:, -1].values, n_estimators=int(n_estimators), outputfile=outputfile, stop_time=int(stop_time))
+        compare_results(data=dataset.iloc[:, 0:-1].values, 
+                        target=dataset.iloc[:, -1].values, 
+                        n_estimators=int(n_estimators), 
+                        outputfile=outputfile, 
+                        stop_time=int(stop_time),
+                        n_cores=int(n_cores)
+                       )
     print('It is finished!')
 
 if __name__ == "__main__":
